cb4f729538872908fc83a3aea926122464b91669
Nick Alesbrook <nick.alesbrook@gmail.com>
2019-12-02 17:20:23 +0000

    .

--- AzureNickDB/AzureNickDB/dbo/Tables/NickTab1.sql
+++ AzureNickDB/AzureNickDB/dbo/Tables/NickTab1.sql
@@ -2,5 +2,8 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
     [Name] NCHAR(10) NULL, 
-    [Address] NCHAR(10) NULL
+    [Address] NCHAR(10) NULL, 
+    [Blah] NCHAR(10) NULL, 
+    [Blah2] NCHAR(10) NULL, 
+    [Blah3] NCHAR(10) NULL
 )
