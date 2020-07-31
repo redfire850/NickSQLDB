1d2a7cedf34fc1f250baeeede9b226cd32468e89
Nick Alesbrook <redfire850@gmail.com>
2020-07-31 10:37:38 +0100

    Added Terraform files

--- Terraform/azure_database.tf
+++ Terraform/azure_database.tf
@@ -7,21 +7,21 @@ resource "random_string" "random-name" {
 }
 
 resource "azurerm_sql_server" "demo" {
-  name                         = "sqlserver-${random_string.random-name.result}"
+  name                         = __sqlservername__
   resource_group_name          = azurerm_resource_group.demoSQLrg.name
   location                     = azurerm_resource_group.demoSQLrg.location
   version                      = "12.0"
-  administrator_login          = "sqladmin"
-  administrator_login_password = "Supersix1234"
+  administrator_login          = __sqlserveruser__
+  administrator_login_password = __sqlserverpassword__
 }
 
 resource "azurerm_sql_server" "demo-secondary" {
-  name                         = "sqlserver-s${random_string.random-name.result}"
+  name                         = __sqlservername__
   resource_group_name          = azurerm_resource_group.demoSQLrg.name
   location                     = var.failover_location
   version                      = "12.0"
-  administrator_login          = "sqladmin"
-  administrator_login_password = "Supersix1234"
+  administrator_login          = __sqlserveruser__
+  administrator_login_password = __sqlserverpassword__
 }
 
 resource "azurerm_sql_failover_group" "failover" {
@@ -41,7 +41,7 @@ resource "azurerm_sql_failover_group" "failover" {
 }
 
 resource "azurerm_sql_database" "training" {
-  name                             = "demodb"
+  name                             = __sqlserverdatabasename__
   resource_group_name              = azurerm_resource_group.demoSQLrg.name
   location                         = azurerm_resource_group.demoSQLrg.location
   server_name                      = azurerm_sql_server.demo.name
